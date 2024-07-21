fcdeffd9924a0fc16aa6aff0b1b4a198df27d482
Min Lwin <lwin.zawmin@gmail.com>
2024-07-21 17:18:12 +0630

    Share Hello World Project

---
+++ s00-hello-world/src/main/java/com/jdc/spring/hello/S00HelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.jdc.spring.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S00HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S00HelloWorldApplication.class, args);
+	}
+
+}
---
+++ s00-hello-world/src/test/java/com/jdc/spring/hello/S00HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdc.spring.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S00HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
