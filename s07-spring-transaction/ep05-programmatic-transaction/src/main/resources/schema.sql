975c4808278966fdb5ab0e5c3891a65189aa49c9
Min Lwin <lwin.zawmin@gmail.com>
2024-09-15 10:19:24 +0630

    Share Sale Model Schema

---
+++ s07-spring-transaction/ep05-programmatic-transaction/src/main/resources/schema.sql
@@ -0,0 +1,67 @@
+set foreign_key_checks = 0;
+
+drop table if exists MEMBERS;
+create table MEMBERS (
+	id int primary key auto_increment,
+	name varchar(40) not null,
+	login_id varchar(40) not null,
+	role varchar(10) not null,
+	password varchar(255) not null,
+	regist_at timestamp null default CURRENT_TIMESTAMP
+);
+
+drop table if exists CATEGORY;
+create table CATEGORY (
+	id int primary key auto_increment,
+	name varchar(40) not null
+);
+
+
+drop table if exists PRODUCT;
+
+create table PRODUCT (
+	id int primary key auto_increment,
+	name varchar(40) not null,
+	unit_price int not null,
+	category_id int not null,
+	description varchar(255),
+	foreign key (category_id) references CATEGORY(id)
+);
+
+drop table if exists SALE_HISTORY;
+
+create table SALE_HISTORY (
+	id int primary key auto_increment,
+	members_id int not null,
+	status varchar(20) null default 'Initiated',
+	remark varchar(255),
+	create_at timestamp null default CURRENT_TIMESTAMP,
+	update_at timestamp null default CURRENT_TIMESTAMP,
+	foreign key (members_id) references MEMBERS(id)
+);
+
+drop table if exists SALE;
+
+create table SALE (
+	id int primary key,
+	members_id int not null,
+	sale_at timestamp null default CURRENT_TIMESTAMP,
+	foreign key (id) references SALE_HISTORY(id),
+	foreign key (members_id) references MEMBERS(id)
+);
+
+drop table if exists SALE_ITEM;
+
+create table SALE_ITEM (
+	sale_id int not null,
+	product_id int not null,
+	unit_price int not null,
+	quantity int not null,
+	primary key (sale_id, product_id),
+	foreign key (sale_id) references SALE(id),
+	foreign key (product_id) references PRODUCT(id)
+);
+
+set foreign_key_checks = 1;
+
+
---
+++ s07-spring-transaction/ep05-programmatic-transaction/src/test/java/com/jdc/transaction/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.jdc.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+
+	@Test
+	void loading_test() {
+		
+	}
+}
