8cf3d87a8819f04fc366cdc6b33351fdda17d108
Min Lwin <lwin.zawmin@gmail.com>
2024-10-27 13:22:45 +0630

    Update Insert SQL File

---
+++ s09-spring-webmvc/ep01-springmvc-xml/src/main/resources/region.sql
@@ -0,0 +1,15 @@
+insert into region (name, region, capital) values ('Ayeyarwady Region', 'South', 'Pathein');
+insert into region (name, region, capital) values ('Bago Region', 'South, Central', 'Bago');
+insert into region (name, region, capital) values ('Chin State', 'North, West', 'Hakha');
+insert into region (name, region, capital) values ('Kachin State', 'North', 'Myitkyina');
+insert into region (name, region, capital) values ('Kayah State', 'East', 'Loikaw');
+insert into region (name, region, capital) values ('Kayin State', 'South, East', 'Hpa-an');
+insert into region (name, region, capital) values ('Magway Region', 'Central', 'Magwe');
+insert into region (name, region, capital) values ('Mandalay Region', 'Central', 'Mandalay');
+insert into region (name, region, capital) values ('Mon State', 'South', 'Mawlamyine');
+insert into region (name, region, capital) values ('Naypyidaw Union Territory', 'Central', 'Naypyidaw');
+insert into region (name, region, capital) values ('Rakhine State', 'West', 'Sittwe');
+insert into region (name, region, capital) values ('Sagaing Region', 'North, West', 'Monywa');
+insert into region (name, region, capital) values ('Shan State', 'North, East', 'Taunggyi');
+insert into region (name, region, capital) values ('Tanintharyi Region', 'South', 'Dawei');
+insert into region (name, region, capital) values ('Yangon Region', 'Central', 'Yangon');
