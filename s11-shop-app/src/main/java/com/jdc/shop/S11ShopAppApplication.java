ff24cd6b89980dcc8649c38e26a8634306e86587
Min Lwin <lwin.zawmin@gmail.com>
2024-12-28 14:00:19 +0630

    Share JSP Project

---
+++ s11-shop-app/src/main/java/com/jdc/shop/S11ShopAppApplication.java
@@ -0,0 +1,13 @@
+package com.jdc.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S11ShopAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S11ShopAppApplication.class, args);
+	}
+
+}
---
+++ s11-shop-app/src/test/java/com/jdc/shop/S11ShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdc.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S11ShopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
